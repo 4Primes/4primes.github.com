0a41a5bc271a59e2d3f98ddb53276048e422506a
Fehmi Can Saglam <fehmican.saglam@4primes.com>
2013-02-25 23:28:19 +0200

    Yeni yazı eklendi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,28 @@
 $(document).ready(function() {
-	$(window).scroll(function(){
-		
-		if ($(window).scrollTop() > 35) {
-			 $('.bar').animate({ height: '130px', opacity:0.8 }, 100, function() {
-				$('#logo a').css("display","block");
-			  });
-		} else  {
-			$('.bar').animate({ height: '7px' }, 50, function() { 
-			 	$('#logo a').css("display","none");
-			});
-		}
+  $(window).scroll(function(){
+      
+      if ($(window).scrollTop() > 35) {
+           $('.bar').animate({ height: '130px', opacity:0.8 }, 100, function() {
+              $('#logo a').css("display","block");
+            });
+      } else  {
+          $('.bar').animate({ height: '7px' }, 50, function() { 
+              $('#logo a').css("display","none");
+          });
+      }
 
-		$("#scrollUp").click(function(){
-			 $('html').animate({scrollTop : 0},'slow');
-		})
-
-
-		if($(window).width()>=980) {
-			if ($(window).scrollTop() > 35) {
-					$("#scrollUp").fadeIn();
-			} else  {
-				 	$("#scrollUp").fadeOut();
-			}
-		}
+      $("#scrollUp").on('click', function(e){
+        e.preventDefault();
+        $('body').animate({scrollTop : 0}, 'slow');
+      });
 
 
-
-
-	}); 	
-
-	
+      if($(window).width() >= 980) {
+        if ($(window).scrollTop() > 35) {
+          $("#scrollUp").fadeIn();
+        } else {
+          $("#scrollUp").fadeOut();
+        }
+      }
+  });
 });
