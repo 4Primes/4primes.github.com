68e1a726a2cbb9c373d21f173fdae7771cc353a9
Fehmi Can Saglam <fehmican.saglam@4primes.com>
2013-02-28 22:44:57 +0200

    Yeni stiller

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,21 @@
 $(document).ready(function() {
   $(window).scroll(function(){
-      
-      if ($(window).scrollTop() > 35) {
-           $('.bar').animate({ height: '130px', opacity:0.8 }, 100, function() {
-              $('#logo a').css("display","block");
-            });
-      } else  {
-          $('.bar').animate({ height: '7px' }, 50, function() { 
-              $('#logo a').css("display","none");
-          });
-      }
-
-      $("#scrollUp").on('click', function(e){
-        e.preventDefault();
-        $('body').animate({scrollTop : 0}, 'slow');
+    
+    if ($(window).scrollTop() > 15) {
+       $('.bar').animate({ height: '100px', opacity:0.9 }, 50, function() {
+        $('#logo a').css("background-position","0 -71px");
+        });
+    } else  {
+      $('.bar').animate({ height: '7px' }, 50, function() { 
+        $('#logo a').css("background-position","0 0");
       });
+    }
 
-
-      if($(window).width() >= 980) {
-        if ($(window).scrollTop() > 35) {
-          $("#scrollUp").fadeIn();
-        } else {
-          $("#scrollUp").fadeOut();
-        }
-      }
   });
+
+  $.scrollUp({ 
+    topDistance: '200',
+    topSpeed: 300
+  });
+  
 });
